059d01e2bfe52942cd8173ccdf069113ed5a49ba
DownhillFromNow <63586795+DownhillFromNow@users.noreply.github.com>
2020-04-22 11:51:35 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 var web3 = new Web3(Web3.givenProvider);
 var contractInstance;
+var playerAccount = "";
+var withdrawAmt = 0;
 
 $(document).ready(function() {
     window.ethereum.enable().then(function(accounts){
-        contractInstance = new web3.eth.Contract(window.abi, "0x79f9F38d3Ff5363D2090Be28bc6EA3276F8C2923", {from:accounts[0]}); //abi is found in the People.json file
+        playerAccount = accounts[0];
+        contractInstance = new web3.eth.Contract(window.abi, "0x8F85436E9BC2522cF71cd8b2d669439A4b96F861", {from:playerAccount}); //abi is found in the People.json file
         console.log(contractInstance);
     });
+
     
     $("#deposit-button").click(deposit);
-    $("#flip-coin").click(flipCoin);
-    $("#withdraw").click(withdraw);
+    $("#flip-coin-heads").click(flipCoinHeads);
+    $("#flip-coin-tails").click(flipCoinTails);
+    $("#withdraw").click(getWinnings);
 
     //click handler, executes the function in bracets
     
@@ -35,35 +40,63 @@ function deposit(){
 }
 
 
-function flipCoin(){
+function flipCoinHeads(){
     var betAmount = $("#bet-box").val();
-    var betType = $("#dropdown").val();
 
-    contractInstance.methods.flip(betType, betAmount).send({value:web3.utils.toWei(betAmount, "ether")})
+    var config = {value:web3.utils.toWei(betAmount, "ether"), gas:100000};
+
+    contractInstance.methods.flip(0).send(config)
     .on("transactionHash", function(transactionHash){
-        console.log("transactionHash");
-        console.log(transactionHash); // .on is used to search for listeners
+        console.log("transactionHash: " + transactionHash); // .on is used to search for listeners
     })
     .on("confirmation", function(confirmationNr){
-        console.log("confirmationNr");
-        console.log(confirmationNr);
+        console.log("confirmationNr: " + confirmationNr);
     })
     .on("receipt", function(receipt){
-        console.log("receipt");
-        console.log(receipt);
-        if(events.userWon.returnValues.won){
-            $("#play-output").text("You won!");
-        }else{
-            $("#play-output").text("Unlucky this time!");
-        }
+        console.log("receipt: " + receipt);
     })
 }
 
-function withdraw(){
-    contractInstance.methods.withdraw().call().then(function(res){
-        $("withdraw-output").text(res);
+function flipCoinTails(){
+    var betAmount = $("#bet-box").val();
+
+    var config = {value:web3.utils.toWei(betAmount, "ether"), gas:100000};
+
+    contractInstance.methods.flip(1).send(config)
+    .on("transactionHash", function(transactionHash){
+        console.log("transactionHash: " + transactionHash); // .on is used to search for listeners
+    })
+    .on("confirmation", function(confirmationNr){
+        console.log("confirmationNr: " + confirmationNr);
+    })
+    .on("receipt", function(receipt){
+        console.log("receipt: " + receipt);
     })
 
 }
 
+function getWinnings(){
+    var amtAmount = $("#amt-box").val();
 
+    var config = {from:playerAccount};
+
+    contractInstance.methods.withdraw(web3.utils.toWei(amtAmount, "ether"))
+    .send(config)
+    .on("transactionHash", function(hash) {
+        console.log('transactionHash: ' + hash);
+      })
+      .on("confirmation", function(confNum) {
+        console.log('confirmation: ' + confNum);
+      })
+      .on("receipt", function(receipt){
+        console.log('receipt: ' + receipt);
+        alert("Done");
+      });
+
+}
+
+
+
+function displayInfo(res){
+    $("#myBalance_output").text(Web3.utils.fromWei(res, 'ether'));
+}
