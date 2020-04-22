74de5d13ec32495b62f79825295fb56f27bfb0d0
DownhillFromNow <63586795+DownhillFromNow@users.noreply.github.com>
2020-04-22 11:50:54 +0100

    Update main.sol

--- main.sol
+++ main.sol
@@ -7,8 +7,8 @@ uint public balance;
 
 mapping(address => uint) public availableBalances;
 
-event userWon(address player, bool won);
 event moneyAdded(address player, bool added);
+event coinFlip(address caller, uint256 amount, bool win);
 
 
   modifier costs(uint cost){
@@ -24,25 +24,30 @@ function addBalance()public payable costs(1 ether){
 
 }
 
-function flip(uint betType, uint betAmount) public payable {
+
+function flip(uint betType) public payable returns (bool) {
     require(betType == 1 || betType == 0, "Bet must be heads or tails!"); //Heads will be 0 and Tails will be 1
-    require(betAmount <= availableBalances[msg.sender], "You can only bet as much as your balance, deposit more please!");
-    bool winner = false;
+    require(msg.value <= availableBalances[msg.sender], "You can only bet as much as your balance, deposit more please!");
+    bool winner;
 
     if((now % 2 == 0 && betType == 0) || (now % 2 == 1 && betType == 1)){
-        availableBalances[msg.sender] += (betAmount * 2);
+        availableBalances[msg.sender] += msg.value;
+        balance -= msg.value;
         winner = true;
     }else{
-        availableBalances[msg.sender] -= betAmount;
+        availableBalances[msg.sender] -= msg.value;
+        balance += msg.value;
+        winner = false;
     }
-    emit userWon(msg.sender, winner);
+
+    emit coinFlip(msg.sender, msg.value, winner); 
+    return winner; 
 }
 
-function withdraw() public {
-    require(availableBalances[msg.sender] > 0.01 ether, "Balance must be larger to withdraw!");
-    uint transferAmount = availableBalances[msg.sender];
-    msg.sender.transfer(transferAmount);
+function withdraw(uint amt) public {
+    require(amt <= availableBalances[msg.sender], "Not enough");
     availableBalances[msg.sender] = 0;
+    msg.sender.transfer(amt);
 }
 
 }
